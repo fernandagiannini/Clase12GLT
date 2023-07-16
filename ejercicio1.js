4e0b44d8b3fdda67f139da8b800c26c01ea1542e
Fernanda Giannini <elcambionv@gmail.com>
2023-07-16 20:09:08 -0300

    make ejercicio1

---
+++ ejercicio1.js
@@ -0,0 +1,7 @@
+const arrayElements = [1, 2, 3, 4]
+console.log(arrayElements)
+
+const deleteFirtsElement = (array) => array.shift()
+
+deleteFirtsElement (arrayElements)
+console.log(arrayElements)
